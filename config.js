89055ad8399270a41a89d54df37cfc6d7749f054
h4rrydog <ifung@guanxin.de>
2014-12-26 17:25:07 +0000

    Scrubbed config.js.

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    production: {
+        db: 'mongodb://127.0.0.1:27017/ActiveMe', 
+        fitbitClientKey: 'FitbitClientKey',
+        fitbitClientSecret: 'FitbitClientSecret',
+        host: '127.0.0.1:3000' 
+    }
+};
